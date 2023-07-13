bc1df270f2b69bdbc92e94e3357fc4e74ee728d5
Xiangting Li <xiangting.li@ucla.edu>
2023-07-12 21:03:04 -0700

    add the differentiable 2-Wasserstein distance

--- src/DifferentiableWasserstein.jl
+++ src/DifferentiableWasserstein.jl
@@ -1,5 +1,4 @@
 module DifferentiableWasserstein
-
 function W₁(u_samples, v_samples)
     # adapted from https://github.com/nklb/wasserstein-distance
     u_samples_sorted = sort(u_samples);
@@ -15,5 +14,22 @@ end
 
 cdf(u_samples) = x -> sum(u_samples .<= x) / length(u_samples)
 
-export W₁
+function W₂(u_samples, v_samples)
+    # adapted from https://github.com/nklb/wasserstein-distance
+    u_samples_sorted = sort(u_samples);
+    v_samples_sorted = sort(v_samples);    
+    u_icdf_grids = [i / length(u_samples) for i in 0:length(u_samples)]
+    v_icdf_grids = [i / length(v_samples) for i in 0:length(v_samples)]
+    grids = unique([u_icdf_grids; v_icdf_grids]) |> sort
+    U_icdf = quantile(u_samples).(grids[1:end-1])
+    V_icdf = quantile(v_samples).(grids[1:end-1])
+    return sqrt(sum((U_icdf - V_icdf).^2 .* diff(grids)))
+end
+
+function quantile(samples)
+    # return a function that computes the quantile of a given sample
+    samples_sorted = sort(samples)
+    return p -> samples_sorted[floor(Int, p * length(samples_sorted))+1]
+end
+export W₁, W₂
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Random
 using PyCall
 using Zygote
 using Flux
-@pyimport scipy.stats as stats
+using ExactOptimalTransport
 
 @testset "DifferentiableWasserstein.jl" begin
 
@@ -13,7 +13,7 @@ using Flux
     for i in 1:1000
         u_samples = rand(1000)
         v_samples = rand(1000)
-        if !(W₁(u_samples, v_samples) ≈ stats.wasserstein_distance(u_samples, v_samples))
+        if !(W₁(u_samples, v_samples) ≈ ExactOptimalTransport.wasserstein(discretemeasure(u_samples), discretemeasure(v_samples)))
             flag = false
         end
     end
@@ -42,4 +42,30 @@ using Flux
     end
 
     @test (pars[2] - 1.0) < 0.1
+
+
+    Random.seed!(1234)
+    pars = [1.0,2.0]
+    u_samples = randn(2000)
+    v_samples = randn(1000)
+    function f(pars, u_samples, v_samples)
+        par1, par2 = pars
+        return W₂(u_samples, v_samples .* par2)
+    end
+
+    grads = gradient(Flux.params(pars)) do
+        f(pars, u_samples, v_samples)
+    end
+
+    opt = ADAM(0.01)
+
+    for i in 1:1000
+        grads = gradient(Flux.params(pars)) do
+            f(pars, u_samples, v_samples)
+        end
+        Flux.update!(opt, Flux.params(pars), grads)
+    end
+
+    @test (pars[2] - 1.0) < 0.1
+
 end
